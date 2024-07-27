93f85de27219ae2f7a83a8849710683d657dd7b9
Aufafikri <fikriilmi6@gmail.com>
2024-07-27 21:42:24 +0700

    deployments

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [forwardRef(() => AuthModule)],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService]
