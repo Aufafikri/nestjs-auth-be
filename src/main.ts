f08aa398f5e3e05dee5a1be16f4a60f2069a7657
Aufafikri <fikriilmi6@gmail.com>
2024-07-28 00:22:24 +0700

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.enableCors({
     origin: ["http://localhost:3000", "https://evstauth.vercel.app"],
     methods: "GET, POST, PUT, DELETE",
