002f5827279149e1749496db0e28e753557b3bfe
Aufafikri <fikriilmi6@gmail.com>
2024-07-27 21:53:35 +0700

    deployments

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: ["http://localhost:3000", "https://evstauth.vercel.app"],
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true
+  })
   await app.listen(5000);
 }
 bootstrap();
