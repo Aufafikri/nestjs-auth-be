4786a8d8f0ed97da989d4684d8cb99b704228fe1
Aufafikri <fikriilmi6@gmail.com>
2024-07-28 13:22:18 +0700

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors()
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ["http://localhost:3000", "https://evstauth.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Accept"],
+    credentials: true
+  });
   await app.listen(5000);
 }
 bootstrap();
