6cceaf15cf80fdec0f9ce0bcc33b0ff5a193026a
Aufafikri <fikriilmi6@gmail.com>
2024-07-28 15:12:37 +0700

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
-  app.enableCors({
-    origin: ["http://localhost:3000", "https://evstauth.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Accept"],
-    credentials: true
-  });
+  // app.use(function(req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  //   res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // });
+
+  // app.enableCors({
+  //   origin: ["http://localhost:3000", "https://evstauth.vercel.app"],
+  //   methods: ["GET", "POST", "PUT", "DELETE"],
+  //   allowedHeaders: ["Content-Type", "Accept"],
+  //   credentials: true
+  // });
+
+  app.use(cors())
+
   await app.listen(5000);
 }
 bootstrap();
